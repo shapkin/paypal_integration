cfe6188a892ab8e55c45b8a078a70df67eaedb2f
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-03-15 16:53:33 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'devise'
+gem 'bootstrap-sass'
+gem 'paypal_adaptive'
+gem 'country_select'
+gem 'configatron'
+
+group :development, :test do
+  gem 'mysql2'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'execjs'
+  gem 'therubyracer'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :production do
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalIntegration::Application.config.secret_token = 'cd434972b8c67551f74828ef70d3d8412a6478cc08c35edabc62cca66d60d02ea5f50b7db0dca4bc5abc5b2d89596c15a9444d091c93c888e8666bf51f321377'
